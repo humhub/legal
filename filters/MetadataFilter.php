f9d69818ba0966e381900fb608f3f815aef20af5
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2024-03-29 13:48:39 -0400

    Update MetadataFilter.php

--- filters/MetadataFilter.php
+++ filters/MetadataFilter.php
@@ -5,8 +5,6 @@ namespace humhub\modules\legal\filters;
 use Yii;
 
 /**
- * Class MetadataFilter
- *
  * Provides methods to filter out unneeded metadata from the provided content.
  */
 class MetadataFilter
@@ -15,13 +13,48 @@ class MetadataFilter
      * Filter out unneeded metadata from the provided content.
      *
      * @param array $content The array of content items to be filtered.
-     * @return array The filtered content array with comments removed.
+     * @return array The filtered content array with unneeded metadata removed.
      */
     public static function filterMetadata($content)
     {
         return array_map(function ($item) {
-            // Remove comments completely
-            unset($item['content']['comments']);
+            // Remove URL from user metadata if it exists
+            if (isset($item['user']['url'])) {
+                unset($item['url']);
+            }
+
+            // Remove comments, likes, and files from content metadata if they exist
+            if (isset($item['content']['comments'])) {
+                unset($item['content']['comments']);
+                unset($item['content']['files']);
+                unset($item['content']['likes']);
+                unset($item['content']['metadata']['created_by']['url']);
+                unset($item['content']['metadata']['updated_by']['url']);
+            }
+
+            // Remove URL from comment metadata if it exists
+            if (isset($item['createdBy']['url'])) {
+                unset($item['createdBy']['url']);
+                unset($item['updatedBy']['url']);
+                unset($item['likes']);
+                unset($item['files']);
+            }
+
+            // Remove URL from file metadata if it exists
+            if (isset($item['file'])) {
+                foreach ($item['file'] as &$file) {
+                    unset($file['url']);
+                }
+            }
+
+            // Remove URL from like metadata if it exists
+            if (isset($item['like'])) {
+                foreach ($item['like'] as &$like) {
+                    unset($like['createdBy']['url']);
+                    unset($like['updatedBy']['url']);
+                }
+            }
+
             return $item;
         }, $content);
     }
